c510f03a39c393948a2e78927cc487742356f8db
Windows <Windows@User>
2016-03-18 22:02:37 +0700

    asdas

--- src/sistem/informasi/data/barang/Petugas.java
+++ src/sistem/informasi/data/barang/Petugas.java
@@ -11,6 +11,8 @@ package sistem.informasi.data.barang;
  */
 public class Petugas extends Orang {
 
+    Gudang G = new Gudang(112,20,"Gudang Barang");
+    
     @Override
     public String getNama() {
         return super.nama;
@@ -38,15 +40,72 @@ public class Petugas extends Orang {
     public Petugas(){
     }
     
-    public void addBarang(){
+    public void addBarang(String nama, int jumlah, Penyedia p){
+        for(int i=0 ; i < p.getIndex(); i++){
+            if (p.getDaftarBarang()[i].getNama_barang().equals(nama)){//p.getDaftarBarang()[i] adalah algoritma untuk mendapatkan barang dari indeks ke i dengan 3 atribut. bila di dot get nama artinya untuk mendapatkan nama dari suatu barang tersebut.
+                Barang b = p.getDaftarBarang()[i]; // create objek barang, barang sudah di dapatkan dari penyedia
+                if (jumlah < b.getJumlah_stok()) {
+                    b.setJumlah_stok(jumlah); // mengeset jumlah barang yang akan di terima petugas dari penyedia. dengan menggunakan jumlah barang si petugas.
+                    G.addBarang(b);
+                    break;
+                }
+                
+                
+            } 
+            
+        }
     }
     
-    public void hapusBarang(){
+    public void hapusBarang(String nama){
+        int temp = 0;
+        Barang br = new Barang();
+        for(int i=0 ; i< G.getIndeks(); i++){
+           if (G.getBarangGudang()[i].getNama_barang().equals(nama)){
+               temp = i;
+               br = G.getBarangGudang()[i];
+               break;
+           }
+        }
+        for(int i=temp ; i< G.getIndeks(); i++){
+           G.getBarangGudang()[i] = G.getBarangGudang()[i+1];  
+        }
+        G.setIndeks(G.getIndeks()-1);
+        G.setKapasitas(G.getKapasitas()+br.getJumlah_stok());
     }
     
-    public void editBarang(){
+    public void editBarang(String nama, int jumlah){
+        int stok_lama = 0;
+        int temp = 0;
+        Barang br = new Barang();
+        if (jumlah < G.getKapasitas()){
+        for(int i=0 ; i< G.getIndeks(); i++){
+           if (G.getBarangGudang()[i].getNama_barang().equals(nama)){
+               temp = i;
+               br = G.getBarangGudang()[i];
+               stok_lama = G.getBarangGudang()[i].getJumlah_stok();
+               break;
+           }
+        }
+        br.setJumlah_stok(jumlah);
+        G.getBarangGudang()[temp] = br;
+        if(br.getJumlah_stok() < stok_lama){
+            G.setKapasitas(G.getKapasitas()+(stok_lama-br.getJumlah_stok()));
+        } else if(br.getJumlah_stok() > stok_lama){
+           G.setKapasitas(G.getKapasitas()-(br.getJumlah_stok()-stok_lama));
+        }
     }
+  }
     
-    public void lihatBarang(){
+    public void lihatBarang(String nama){
+        int temp = 0;
+        Barang br = new Barang();
+        for(int i=0 ; i< G.getIndeks(); i++){
+           if (G.getBarangGudang()[i].getNama_barang().equals(nama)){
+               temp = i;
+               br = G.getBarangGudang()[i];
+               System.out.println("Nama: "+br.getNama_barang());
+               break;
+           }
+        }
     }
 }
